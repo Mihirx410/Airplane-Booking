4242b25c7c224f6d0a940bad18da5052f09f45ff
Mihir Dodiya <mihirdodiya0077@gmail.com>
2025-06-28 17:12:58 +0530

    login and booking

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from '../pages/Login'
-import LandingPage from '../pages/LandingPage'
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Login />} />
       </Routes>
