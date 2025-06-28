0a1f46f2b8c309ef6edacfdb1b9d7892da7bdb2d
Mihir Dodiya <mihirdodiya0077@gmail.com>
2025-06-28 17:19:30 +0530

    minor route issue

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from '../pages/Login'
+import LandingPage from '../pages/LandingPage'
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Login />} />
       </Routes>
